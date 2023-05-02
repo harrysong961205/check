cba6440c0f39aca9c1254042447aa0301b948f8e
harrysong961205 <fkdlzldeja@gmail.com>
2023-05-03 02:37:09 +0900

    Add 장고 서버 예시 추가

--- ocr.py
+++ ocr.py
@@ -2,7 +2,7 @@ import pytesseract
 import cv2 
 import matplotlib.pyplot as plt
 
-path = 'ocr_test.jpeg'
+path = 'jpg_test/ocr_test.jpeg'
 image = cv2.imread(path)
 rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
